d74382bf5680956ec4d52800f712331d4587ff23
Ruben <neburtl837@gmail.com>
2026-03-23 15:19:50 +0100

    Refactor proyect with Lenis

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Lenis from "lenis";
 import "./styles/index.css";
 import App from "./App.tsx";
+
+// Inicializar Lenis para smooth scrolling
+const lenis = new Lenis({
+  duration: 1.6,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+  smoothTouch: true,
+  
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
